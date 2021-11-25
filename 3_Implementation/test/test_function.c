4be98ae62a746b60a37a5510466a7376bfced022
SHANMUGAAPRIYANM <94114144+SHANMUGAAPRIYANM@users.noreply.github.com>
2021-11-25 14:33:20 +0530

    Update test_function.c

--- 3_Implementation/test/test_function.c
+++ 3_Implementation/test/test_function.c
@@ -11,16 +11,16 @@ void test_print all the day of month(void)
 {
 TEST_ASSERT_EQUAL(pass,displaymonth(1));
 }
-void test_addnote(void){
+void test_add note(void){
 TEST_ASSERT_EQUAL(pass,Note added(1));
 }
 int main()
 {
 UNITY_BEGIN();
 
-    RUN_TEST(test_);
-    RUN_TEST(test_);
-    RUN_TEST(test_);
+    RUN_TEST(test_Find out the day);
+    RUN_TEST(test_print all the day of month);
+    RUN_TEST(test_add note);
 
 return UNITY_END();
 }
